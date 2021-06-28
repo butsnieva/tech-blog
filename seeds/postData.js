1941df96a39c4e62586efd400d6580640b3bc42e
Kateryna B <butsneva.ekaterina@gmail.com>
2021-06-27 23:58:04 -0500

    add post seeds and views

---
+++ seeds/postData.js
@@ -0,0 +1,35 @@
+const { Post } = require('../models');
+
+const postData = [
+    {
+        title: 'Title 1',
+        content: `Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system.`,
+        user_id: 1,
+    },
+    {
+        title: 'Title 2',
+        content:`Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system.
+                Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system.`,
+        user_id: 1,
+    },
+    {
+        title: 'Title 3',
+        content: `Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system.`,
+        user_id: 1,
+    },
+    {
+        title: 'Title 4',
+        content: `Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system.`,
+        user_id: 1,
+    },
+    {
+        title: 'Title 5',
+        content:`Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system.
+                Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system.`,
+        user_id: 1,
+    }
+];
+
+const seedPost = () => Post.bulkCreate(postData);
+
+module.exports = seedPost;
